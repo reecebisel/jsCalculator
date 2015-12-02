ca55ae0f72c5d03159cfc0d994fb2729e31b8837
Reece <reecebisel@gmail.com>
2015-12-02 15:05:07 -0700

    straight jquery styling for the responsive width and whatnot.

--- calculator2.js
+++ calculator2.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
   var Calculator = {
-    operCodes:  [42, 43, 45, 47, 37],
-    numCodes:   [48, 49, 50, 51, 52, 53, 54, 55, 56, 57],
-    numButtons: $('span.button-cell'),
-    clrButton:  $('span.clear'),
-    eqlButton:  $('span.equals'),
-    eqDiv:      $('.equation'),
-    pstDiv:     $('.past'),
-    rsltDiv:    $('.results'),
-    calcBod:    $('.calc-body'),
-    dispDiv:    $('.display'),
-
+    operCodes:     [42, 43, 45, 47, 37],
+    numCodes:      [48, 49, 50, 51, 52, 53, 54, 55, 56, 57],
+    numButtons:    $('span.button-cell'),
+    clrButton:     $('span.clear'),
+    eqlButton:     $('span.equals'),
+    eqDiv:         $('.equation'),
+    pstDiv:        $('.past'),
+    rsltDiv:       $('.results'),
+    calcBod:       $('.calc-body'),
+    dispDiv:       $('.display'),
+    calBodMod:     {'padding':'0', 'margin':'0'},
+    dispMod:       {'padding': '0', 'margin': '0'},
+    smTxtMod:      {'font-size': '1em', 'line-height': '100%'},
+    eqMod:         {'line-height': '100%', 'font-size': '1.5em', 'vertical-align': 'middle'},
+    buttonMod:     {'line-height': '100%', 'font-size': '1em', 'vertical-align': 'middle'},
+    stndButton:    {'line-height': '100px', 'font-size': '24px', 'vertical-align': 'middle'},
+    
      numClick: function() {
       this.numButtons.click((function(event){
          var element = event.currentTarget;
@@ -81,28 +87,51 @@ $(document).ready(function(){
       this.eqlButton.click(this.equalsFunc.bind(this));
       this.clrButton.click(this.clearFunc.bind(this));
       this.clrButton.dblclick(this.fullClear.bind(this));
+      this.chkWidth();
     }, 
 
-    adjWidth: function() {
+
+    functions below are 
+
+    calcElmChg: function(target, modObj, parentH, numRow) {
+      target.height((parentH / numRow) + 'px');
+      target.css(modObj);
+    },
+
+    chkWidth: function() {
       $(window).resize((function(event) {
         var win = event.currentTarget;
-        if ($(win).width() < 425) {
+        
+        if ($(win).width() < 400) {
           this.calcBod.width($(win).width() + 'px');
+        } else if ($(win).width >= 400) {
+          this.calcBod.css('width', '400px');
         }
+
         if ($(win).height() < 600) {
           winHeight = $(win).height();
-          this.calcBod.height(winHeight + 'px')
-          this.numButtons.height((winHeight / 6) + 'px');
-          this.clrButton.height((winHeight / 6) + 'px');
-          this.eqlButton.height((winHeight / 6) + 'px');
-          dispDivHeight = this.dispDiv.height((winHeight / 6) + 'px');
-          this.eqDiv.height((dispDivHeight / 2) + 'px');
-          this.rsltDiv.height((dispDivHeight / 4) + 'px');
-          this.pstDiv.height((dispDivHeight / 4) + 'px');
+          this.calcBod.height(winHeight + 'px');
+          this.calcBod.css(this.calBodMod);
+          this.calcElmChg(this.numButtons, this.buttonMod, winHeight, 6);
+          this.calcElmChg(this.clrButton, this.buttonMod, winHeight, 6);
+          this.calcElmChg(this.eqlButton, this.buttonMod, winHeight, 6);
+          dispDivHeight = this.dispDiv.height((winHeight / 6));
+          this.dispDiv.height(dispDivHeight + 'px');
+          this.calcElmChg(this.eqDiv, this.eqMod, dispDivHeight, 2);
+          this.calcElmChg(this.rsltDiv, this.smTxtMod, dispDivHeight, 4);
+          this.calcElmChg(this.pstDiv, this.smTxtMod, dispDivHeight, 4);
+        } else if ($(win).height() >= 600) {
+          this.calcBod.css('height', '600px');
+          this.numButtons.css(this.stndButton);
+          this.clrButton.css(this.stndButton);
+          this.eqlButton.css({'line-height': '100px', 'font-size': '24px', 'vertical-align': 'middle', 'width': '100%'});
+          this.dispDiv.css('height', '100px');
+          this.eqDiv.css({'height': '50px', 'font-size': '30px'});
+          this.pstDiv.css({'height': '25px', 'font-size': '16px'});
+          this.rsltDiv.css({'height': '25px', 'font-size': '16px'});
         }
       }).bind(this));
     }
   };
   Calculator.calcInit();
-  Calculator.adjWidth();
 });
